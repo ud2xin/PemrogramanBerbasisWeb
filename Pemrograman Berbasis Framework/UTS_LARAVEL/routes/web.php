abf693003d4ecee73e1135cb605ac2197b0ffade
Muhammad Haikal Syamsuddin <udingaming0903@gmail.com>
2025-09-29 10:36:10 +0700

    menambahkan routes ke web.php (nomor 3 done

--- Pemrograman Berbasis Framework/UTS_LARAVEL/routes/web.php
+++ Pemrograman Berbasis Framework/UTS_LARAVEL/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UTSController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/uts', [UTSController::class, 'index']);
+Route::get('/uts/pemrograman-web', [UTSController::class, 'pemrogramanWeb']);
+Route::get('/uts/database', [UTSController::class, 'database']);
 Route::get('/', function () {
     return view('welcome');
 });
