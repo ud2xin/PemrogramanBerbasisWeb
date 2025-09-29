4def3fcda4be55d10cc94ae0707606cc2d28c048
Muhammad Haikal Syamsuddin <udingaming0903@gmail.com>
2025-09-29 10:41:44 +0700

    nomor 5 done

---
+++ Pemrograman Berbasis Framework/UTS_LARAVEL/resources/views/uts/database.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <h2>Menu UTS Database</h2>
+        <div class="alert alert-success mt-3">
+            <h4 class="alert-heading">Menu UTS Database</h4>
+            <p class="mb-0">Ini adalah halaman untuk menu UTS Database.</p>
+        </div>
+    </div>
+</div>
+@endsection
